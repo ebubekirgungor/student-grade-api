e287f55e94c4effe57d5c432851f53891b6fcf2d
Ebubekir Güngör <ebubekirgungor2003@gmail.com>
2024-07-09 01:40:10 +0300

    Initialize project

---
+++ src/main/java/com/ebubekirgungor/student_grade_api/StudentGradeApiApplication.java
@@ -0,0 +1,13 @@
+package com.ebubekirgungor.student_grade_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradeApiApplication.class, args);
+	}
+
+}
